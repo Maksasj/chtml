969b209a37132044cebf26886fd16a657eb66921
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-05-23 16:42:44 +0300

    more elements !

--- chtml.h
+++ chtml.h
@@ -3,28 +3,52 @@
 
 #include <stdio.h>
 
-#define SCOPE(atStart, atEnd) for (int _scope_break = ((atStart), 1); _scope_break; _scope_break = ((atEnd), 0))
+#define CHTML_SCOPE(atStart, atEnd) for (int _scope_break = ((atStart), 1); _scope_break; _scope_break = ((atEnd), 0))
 
-#define HTML(attributes) SCOPE(fprintf(HTML_STREAM, "<html %s>", attributes), fprintf(HTML_STREAM, "</html>"))
+#define HTML(attributes) CHTML_SCOPE(fprintf(HTML_STREAM, "<html %s>", attributes), fprintf(HTML_STREAM, "</html>"))
 
-#define HEAD(attributes) SCOPE(fprintf(HTML_STREAM, "<head %s>", attributes), fprintf(HTML_STREAM, "</head>"))
-#define BODY(attributes) SCOPE(fprintf(HTML_STREAM, "<body %s>", attributes), fprintf(HTML_STREAM, "</body>"))
+#define HEAD(attributes) CHTML_SCOPE(fprintf(HTML_STREAM, "<head %s>", attributes), fprintf(HTML_STREAM, "</head>"))
+#define BODY(attributes) CHTML_SCOPE(fprintf(HTML_STREAM, "<body %s>", attributes), fprintf(HTML_STREAM, "</body>"))
+#define DIV(attributes) CHTML_SCOPE(fprintf(HTML_STREAM, "<div %s>", attributes), fprintf(HTML_STREAM, "</div>"))
 
 #define H1(format, ...) fprintf(HTML_STREAM, "<h1>" format "</h1>", ##__VA_ARGS__)
+#define H2(format, ...) fprintf(HTML_STREAM, "<h2>" format "</h2>", ##__VA_ARGS__)
+#define H3(format, ...) fprintf(HTML_STREAM, "<h3>" format "</h3>", ##__VA_ARGS__)
+#define H4(format, ...) fprintf(HTML_STREAM, "<h4>" format "</h4>", ##__VA_ARGS__)
+#define H5(format, ...) fprintf(HTML_STREAM, "<h5>" format "</h5>", ##__VA_ARGS__)
+#define H6(format, ...) fprintf(HTML_STREAM, "<h6>" format "</h6>", ##__VA_ARGS__)
+
+#define TABLE(attributes) CHTML_SCOPE(fprintf(HTML_STREAM, "<table %s>", attributes), fprintf(HTML_STREAM, "</table>"))
+#define TR(attributes) CHTML_SCOPE(fprintf(HTML_STREAM, "<tr %s>", attributes), fprintf(HTML_STREAM, "</tr>"))
+#define TD(attributes) CHTML_SCOPE(fprintf(HTML_STREAM, "<td %s>", attributes), fprintf(HTML_STREAM, "</td>"))
+
+#define FORM(attributes) CHTML_SCOPE(fprintf(HTML_STREAM, "<form %s>", attributes), fprintf(HTML_STREAM, "</form>"))
+#define INPUT(format, ...) fprintf(HTML_STREAM, "<input " format ">", ##__VA_ARGS__)
+
+#define BR() fprintf(HTML_STREAM, "<BR>")
+#define HR(format, ...) fprintf(HTML_STREAM, "<hr " format ">", ##__VA_ARGS__)
+
 #define P(format, ...) fprintf(HTML_STREAM, "<p>" format "</p>", ##__VA_ARGS__)
+#define B(format, ...) fprintf(HTML_STREAM, "<b>" format "</b>", ##__VA_ARGS__)
+#define STRONG(format, ...) fprintf(HTML_STREAM, "<strong>" format "</strong>", ##__VA_ARGS__)
+
+#define A(format, ...) CHTML_SCOPE(fprintf(HTML_STREAM, "<a " format ">", ##__VA_ARGS__), fprintf(HTML_STREAM, "</a>"))
+#define SPAN(attributes) CHTML_SCOPE(fprintf(HTML_STREAM, "<span %s>", attributes), fprintf(HTML_STREAM, "</span>"))
+#define STRING(format, ...) fprintf(HTML_STREAM, format, ##__VA_ARGS__)
+
 #define TITLE(format, ...) fprintf(HTML_STREAM, "<title>" format "</title>", ##__VA_ARGS__)
 #define META(attributes, ...) fprintf(HTML_STREAM, "<meta " attributes ">", ##__VA_ARGS__)
 
-#define HTML_BEGIN()\
-    size_t size;    \
-    char *buffer;   \
-                    \
-    FILE* HTML_STREAM = open_memstream(&buffer, &size); \
-    if (HTML_STREAM == NULL) {  \
-        perror("Error opening memory stream");  \
-    }   \
+#define HTML_BEGIN()                                                            \
+    size_t CHTML_BUFFER_SIZE;                                                   \
+    char *CHTML_BUFFER;                                                         \
+                                                                                \
+    FILE* HTML_STREAM = open_memstream(&CHTML_BUFFER, &CHTML_BUFFER_SIZE);      \
+    if (HTML_STREAM == NULL) {                                                  \
+        perror("Error opening memory stream");                                  \
+    }                                                                           \
 
-#define HTML_COMPILE() (fclose(HTML_STREAM), buffer)
+#define HTML_COMPILE() (fclose(HTML_STREAM), CHTML_BUFFER)
 
 
 #endif
